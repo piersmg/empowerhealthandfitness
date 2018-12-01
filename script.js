9a4163e3bfc81fa483cca1d9bebe01bef5f3020e
piersmg <piersgarthwaite@hotmail.co.uk>
2018-12-01 16:39:31 +1100

    Uploading files to Git

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+	var $hamburger = $(".hamburger");
+  $hamburger.on("click", function(e) {
+  	// e.preventDefault();
+    e.stopPropagation();
+
+    $hamburger.toggleClass("is-active");
+    $(".navigation, .nav-menu").toggleClass("is-open");
+
+    $(document).one('click', function closeMenu (e){
+        if($('.navigation, .nav-menu').has(e.target).length === 0){
+            $('.navigation, .nav-menu').removeClass('is-open');
+            $hamburger.removeClass("is-active");
+        } else {
+            $(document).one('click', closeMenu);
+        }
+    });
+  });
+
+  $(".home-blocks").hover(function() {
+  	$(this).children('p').toggleClass("block-hover");
+  });
+
+});
